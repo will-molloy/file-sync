b31d0e66053dfbf2131dd457458831353cf566ae
Will Molloy <willjoemolloy@gmail.com>
2023-05-12 17:32:45 +1200

    Add 3 simple integration tests which cover create/update/delete
    Exposes the bug (issue #2).
    
    Kept it simple since unit tests cover most of the logic.
    
    Still nice to have an integration test which covers everything (i.e.
    without mocks.
    Also integration test is nice because it won't have to change (as often)
    when the main code changes - I find it a bit error prone when you change
    something and end up changing a bunch of tests at the same time. This is
    another layer of defense essentially.
    
    Can't really test 'file same' here as it'd require setting last-modified
    time... want to keep it simple.

--- file-backup-local/src/integrationTest/java/com/willmolloy/backup/local/LocalBackupIntegrationTest.java
+++ file-backup-local/src/integrationTest/java/com/willmolloy/backup/local/LocalBackupIntegrationTest.java
@@ -1,8 +1,207 @@
 package com.willmolloy.backup.local;
 
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import com.willmolloy.backup.BackupRunner;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * LocalBackupIntegrationTest.
  *
+ * <p>Quite simple as most logic is covered by unit tests.
+ *
  * @author <a href=https://willmolloy.com>Will Molloy</a>
  */
-class LocalBackupIntegrationTest {}
+class LocalBackupIntegrationTest {
+
+  private FileSystem fs;
+  private Path sourceRoot;
+  private Path destRoot;
+  private LocalBackup sut;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    fs = Jimfs.newFileSystem(Configuration.forCurrentPlatform());
+    sourceRoot = Files.createDirectory(fs.getPath("Documents"));
+    destRoot = Files.createDirectory(fs.getPath("Backup"));
+    sut = new LocalBackup(new LocalStorage(sourceRoot), new LocalStorage(destRoot));
+  }
+
+  @AfterEach
+  void tearDown() throws IOException {
+    fs.close();
+  }
+
+  @Test
+  void createsFilesOnDestination() throws IOException {
+    // Given
+    // simple file
+    createFile(sourceRoot.resolve("A.txt"), "source text");
+    // empty directory
+    createDirectory(sourceRoot.resolve("B/C"));
+    // directory with multiple files
+    createFile(sourceRoot.resolve("D/E.mp4"), "source video");
+    createFile(sourceRoot.resolve("D/F.mp3"), "source audio");
+    // file nested deep
+    createFile(sourceRoot.resolve("X/Y/Z.pdf"), "source pdf");
+
+    // When
+    BackupRunner.OverallStatistics statistics = BackupRunner.run(sut);
+
+    // Then
+    assertThat(statistics)
+        .isEqualTo(
+            new BackupRunner.OverallStatistics(
+                new BackupRunner.Statistics(4, 0, 0, 0, 45, 0),
+                new BackupRunner.ErrorStatistics(0, 0, 0)));
+
+    assertThat(Files.walk(sourceRoot))
+        .containsExactly(
+            sourceRoot,
+            sourceRoot.resolve("A.txt"),
+            sourceRoot.resolve("B"),
+            sourceRoot.resolve("B/C"),
+            sourceRoot.resolve("D"),
+            sourceRoot.resolve("D/E.mp4"),
+            sourceRoot.resolve("D/F.mp3"),
+            sourceRoot.resolve("X"),
+            sourceRoot.resolve("X/Y"),
+            sourceRoot.resolve("X/Y/Z.pdf"));
+    assertThat(Files.walk(destRoot))
+        .containsExactly(
+            destRoot,
+            destRoot.resolve("A.txt"),
+            destRoot.resolve("D"),
+            destRoot.resolve("D/E.mp4"),
+            destRoot.resolve("D/F.mp3"),
+            destRoot.resolve("X"),
+            destRoot.resolve("X/Y"),
+            destRoot.resolve("X/Y/Z.pdf"));
+
+    assertThat(Files.readString(sourceRoot.resolve("A.txt"))).isEqualTo("source text");
+    assertThat(Files.readString(sourceRoot.resolve("D/E.mp4"))).isEqualTo("source video");
+    assertThat(Files.readString(sourceRoot.resolve("D/F.mp3"))).isEqualTo("source audio");
+    assertThat(Files.readString(sourceRoot.resolve("X/Y/Z.pdf"))).isEqualTo("source pdf");
+
+    assertThat(Files.readString(destRoot.resolve("A.txt"))).isEqualTo("source text");
+    assertThat(Files.readString(destRoot.resolve("D/E.mp4"))).isEqualTo("source video");
+    assertThat(Files.readString(destRoot.resolve("D/F.mp3"))).isEqualTo("source audio");
+    assertThat(Files.readString(destRoot.resolve("X/Y/Z.pdf"))).isEqualTo("source pdf");
+  }
+
+  @Test
+  void updatesFilesOnDestination() throws IOException {
+    // Given
+    // simple file
+    createFile(sourceRoot.resolve("A.txt"), "source text");
+    // empty directory
+    createDirectory(sourceRoot.resolve("B/C"));
+    // directory with multiple files
+    createFile(sourceRoot.resolve("D/E.mp4"), "source video");
+    createFile(sourceRoot.resolve("D/F.mp3"), "source audio");
+    // file nested deep
+    createFile(sourceRoot.resolve("X/Y/Z.pdf"), "source pdf");
+
+    // simple file
+    createFile(destRoot.resolve("A.txt"), "dest text");
+    // empty directory
+    createDirectory(destRoot.resolve("B/C"));
+    // directory with multiple files
+    createFile(destRoot.resolve("D/E.mp4"), "dest video");
+    createFile(destRoot.resolve("D/F.mp3"), "dest audio");
+    // file nested deep
+    createFile(destRoot.resolve("X/Y/Z.pdf"), "dest pdf");
+
+    // When
+    BackupRunner.OverallStatistics statistics = BackupRunner.run(sut);
+
+    // Then
+    assertThat(statistics)
+        .isEqualTo(
+            new BackupRunner.OverallStatistics(
+                new BackupRunner.Statistics(0, 4, 0, 0, 45, 37),
+                new BackupRunner.ErrorStatistics(0, 0, 0)));
+
+    assertThat(Files.walk(sourceRoot))
+        .containsExactly(
+            sourceRoot,
+            sourceRoot.resolve("A.txt"),
+            sourceRoot.resolve("B"),
+            sourceRoot.resolve("B/C"),
+            sourceRoot.resolve("D"),
+            sourceRoot.resolve("D/E.mp4"),
+            sourceRoot.resolve("D/F.mp3"),
+            sourceRoot.resolve("X"),
+            sourceRoot.resolve("X/Y"),
+            sourceRoot.resolve("X/Y/Z.pdf"));
+    assertThat(Files.walk(destRoot))
+        .containsExactly(
+            destRoot,
+            destRoot.resolve("A.txt"),
+            destRoot.resolve("D"),
+            destRoot.resolve("D/E.mp4"),
+            destRoot.resolve("D/F.mp3"),
+            destRoot.resolve("X"),
+            destRoot.resolve("X/Y"),
+            destRoot.resolve("X/Y/Z.pdf"));
+
+    assertThat(Files.readString(sourceRoot.resolve("A.txt"))).isEqualTo("source text");
+    assertThat(Files.readString(sourceRoot.resolve("D/E.mp4"))).isEqualTo("source video");
+    assertThat(Files.readString(sourceRoot.resolve("D/F.mp3"))).isEqualTo("source audio");
+    assertThat(Files.readString(sourceRoot.resolve("X/Y/Z.pdf"))).isEqualTo("source pdf");
+
+    assertThat(Files.readString(destRoot.resolve("A.txt"))).isEqualTo("source text");
+    assertThat(Files.readString(destRoot.resolve("D/E.mp4"))).isEqualTo("source video");
+    assertThat(Files.readString(destRoot.resolve("D/F.mp3"))).isEqualTo("source audio");
+    assertThat(Files.readString(destRoot.resolve("X/Y/Z.pdf"))).isEqualTo("source pdf");
+  }
+
+  @Test
+  void deletesFilesAndDirectoriesOnDestination() throws IOException {
+    // Given
+    // simple file
+    createFile(destRoot.resolve("A.txt"), "dest text");
+    // empty directory
+    createDirectory(destRoot.resolve("B/C"));
+    // directory with multiple files
+    createFile(destRoot.resolve("D/E.mp4"), "dest video");
+    createFile(destRoot.resolve("D/F.mp3"), "dest audio");
+    // file nested deep
+    createFile(destRoot.resolve("X/Y/Z.pdf"), "dest pdf");
+
+    // When
+    BackupRunner.OverallStatistics statistics = BackupRunner.run(sut);
+
+    // Then
+    assertThat(statistics)
+        .isEqualTo(
+            new BackupRunner.OverallStatistics(
+                new BackupRunner.Statistics(0, 0, 4, 0, 0, 37),
+                new BackupRunner.ErrorStatistics(0, 0, 0)));
+
+    assertThat(Files.walk(sourceRoot)).containsExactly(sourceRoot);
+    assertThat(Files.walk(destRoot)).containsExactly(destRoot);
+  }
+
+  private void createFile(Path path, String contents) throws IOException {
+    Path parent = path.getParent();
+    if (parent != null) {
+      Files.createDirectories(parent);
+    }
+    Files.createFile(path);
+    Files.writeString(path, contents);
+  }
+
+  private void createDirectory(Path path) throws IOException {
+    Files.createDirectories(path);
+  }
+}
