444bbfb992c027eaffa5c0e2b8999c1cb8354686
Will Molloy <willjoemolloy@gmail.com>
2023-05-19 22:04:53 +1200

    Test other `FileTree` methods
    Going to replace the map with custom data structure, so the tests are
    valuable.

--- file-backup-core/src/test/java/com/willmolloy/backup/FileTreeTest.java
+++ file-backup-core/src/test/java/com/willmolloy/backup/FileTreeTest.java
@@ -1,10 +1,15 @@
 package com.willmolloy.backup;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,7 +19,49 @@ import org.junit.jupiter.api.Test;
  */
 class FileTreeTest {
 
-  // no reason to test all the methods, most just delegate to the underlying map
+  @Test
+  void forEach_visitsEachNodeExactlyOnce() {
+    // Given
+    var expected = Stream.of("A", "A/B", "A/B/C", "D", "D/E", "D/F").map(s -> file(s)).toList();
+    FileTree fileTree =
+        FileTree.from(
+            expected.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+
+    // When
+    var actual = new ArrayList<>();
+    fileTree.forEach((key, file) -> actual.add(Map.entry(key, file)));
+
+    // Then
+    assertThat(actual).containsExactlyElementsIn(expected);
+  }
+
+  @Test
+  void get_presentWhenNodePresent() {
+    var expected = file("A/B");
+    FileTree fileTree = FileTree.from(Map.ofEntries(file("A"), expected));
+    assertThat(fileTree.get(expected.getKey())).hasValue(expected.getValue());
+  }
+
+  @Test
+  void get_emptyWhenNodeAbsent() {
+    var notExpected = file("A/B");
+    FileTree fileTree = FileTree.from(Map.ofEntries(file("A")));
+    assertThat(fileTree.get(notExpected.getKey())).isEmpty();
+  }
+
+  @Test
+  void contains_trueWhenNodePresent() {
+    var expected = file("A/B");
+    FileTree fileTree = FileTree.from(Map.ofEntries(file("A"), expected));
+    assertThat(fileTree.contains(expected.getKey())).isTrue();
+  }
+
+  @Test
+  void contains_falseWhenNodeAbsent() {
+    var notExpected = file("A/B");
+    FileTree fileTree = FileTree.from(Map.ofEntries(file("A")));
+    assertThat(fileTree.contains(notExpected.getKey())).isFalse();
+  }
 
   @Test
   void containsParentOf_trueWhenParentPresent() {
@@ -52,7 +99,45 @@ class FileTreeTest {
     assertThat(fileTree.containsAnyChildOf(Path.of("A"))).isTrue();
   }
 
+  @Test
+  void fileCount_countsFiles() {
+    FileTree fileTree =
+        FileTree.from(
+            Map.ofEntries(
+                directory("A"),
+                directory("A/B"),
+                file("A/B/C"),
+                file("A/B/D"),
+                directory("X"),
+                directory("X/Y"),
+                file("X/Y/Z")));
+    assertThat(fileTree.fileCount()).isEqualTo(3);
+  }
+
+  @Test
+  void totalSize_sumsFileSize() {
+    FileTree fileTree =
+        FileTree.from(
+            Map.ofEntries(
+                directory("A"),
+                directory("A/B"),
+                file("A/B/C"),
+                file("A/B/D"),
+                directory("X"),
+                directory("X/Y"),
+                file("X/Y/Z")));
+    assertThat(fileTree.totalSize()).isEqualTo(6);
+  }
+
   private static Map.Entry<Path, FileTree.File> file(String path) {
-    return Map.entry(Path.of(path), mock(FileTree.File.class));
+    FileTree.File file = mock(FileTree.File.class);
+    when(file.size()).thenReturn(2L);
+    return Map.entry(Path.of(path), file);
+  }
+
+  private static Map.Entry<Path, FileTree.File> directory(String path) {
+    FileTree.File dir = mock(FileTree.File.class);
+    when(dir.isDirectory()).thenReturn(true);
+    return Map.entry(Path.of(path), dir);
   }
 }
