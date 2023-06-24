419e25d0d074cc53d0038ae9355df8afd1014998
Will Molloy <willjoemolloy@gmail.com>
2023-06-24 17:09:10 +1200

    Try `StructuredTaskScope` with `ShutdownOnFailure`
    Expecting error log rather than silence and partial scan... then can see
    what the problem is??
    
    Also idea for fixed-pool: `Semaphore`!

--- file-sync-local/src/main/java/com/willmolloy/sync/local/LocalStorage.java
+++ file-sync-local/src/main/java/com/willmolloy/sync/local/LocalStorage.java
@@ -7,21 +7,20 @@ import com.google.common.base.Throwables;
 import com.willmolloy.sync.FileTree;
 import com.willmolloy.sync.Location;
 import com.willmolloy.sync.util.docker.DockerHelper;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinWorkerThread;
-import java.util.concurrent.RecursiveAction;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 import java.util.function.BiConsumer;
+import jdk.incubator.concurrent.StructuredTaskScope;
+import jdk.incubator.concurrent.StructuredTaskScope.ShutdownOnFailure;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -59,27 +58,23 @@ public final class LocalStorage implements Location<LocalFile> {
             builder.insert(file);
           };
 
-      try (ForkJoinPool pool = forkJoinPool()) {
-        ParallelWalk walk = new ParallelWalk(rootDir, consumer);
-        pool.invoke(walk);
+      try (ShutdownOnFailure scope =
+          new ShutdownOnFailure(null, Thread.ofVirtual().name("scan-worker-", 1).factory())) {
+        new ParallelWalk(rootDir, consumer, scope).walk();
+        scope.joinUntil(Instant.now().plus(Duration.ofHours(1)));
+        scope.throwIfFailed();
       }
+
       return builder.build();
-    } catch (RuntimeException | IOException e) {
+    } catch (RuntimeException
+        | IOException
+        | ExecutionException
+        | InterruptedException
+        | TimeoutException e) {
       log.error("Error scanning: [{}]", this, e);
       Throwables.throwIfUnchecked(e);
       throw new RuntimeException(e);
     }
-  }
-
-  private static ForkJoinPool forkJoinPool() {
-    ForkJoinPool.ForkJoinWorkerThreadFactory factory =
-        pool -> {
-          ForkJoinWorkerThread worker =
-              ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool);
-          worker.setName("scan-worker-%d".formatted(worker.getPoolIndex()));
-          return worker;
-        };
-    return new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 10, factory, null, false);
   }
 
   public Path root() {
@@ -91,74 +86,56 @@ public final class LocalStorage implements Location<LocalFile> {
     return "%s[%s]".formatted(getClass().getSimpleName(), displayRootDir);
   }
 
-  @SuppressFBWarnings(value = {"SE_BAD_FIELD", "SE_NO_SERIALVERSIONID"})
-  private static final class ParallelWalk extends RecursiveAction {
-    private final Path dir;
-    private final BiConsumer<Path, BasicFileAttributes> consumer;
+  private record ParallelWalk(
+      Path dir, BiConsumer<Path, BasicFileAttributes> consumer, StructuredTaskScope<Object> scope) {
 
-    private ParallelWalk(Path dir, BiConsumer<Path, BasicFileAttributes> consumer) {
-      this.dir = dir;
-      this.consumer = consumer;
-    }
-
-    @Override
-    protected void compute() {
-      List<ParallelWalk> forks = new ArrayList<>();
-      try {
-        Files.walkFileTree(
-            dir,
-            new FileVisitor<>() {
-              @Override
-              public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attributes) {
-                if (ParallelWalk.this.dir == dir) {
-                  log.debug("visit({})", dir);
-                  consumer.accept(dir, attributes);
-                  return FileVisitResult.CONTINUE;
-                } else {
-                  // fork for each new directory discovered
-                  ParallelWalk fork = new ParallelWalk(dir, consumer);
-                  fork.fork();
-                  forks.add(fork);
-                  return FileVisitResult.SKIP_SUBTREE;
-                }
+    private Void walk() throws IOException {
+      Files.walkFileTree(
+          dir,
+          new FileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attributes) {
+              if (ParallelWalk.this.dir == dir) {
+                log.debug("visit({})", dir);
+                consumer.accept(dir, attributes);
+                return FileVisitResult.CONTINUE;
+              } else {
+                // fork for each new directory discovered
+                scope.fork(() -> new ParallelWalk(dir, consumer, scope).walk());
+                return FileVisitResult.SKIP_SUBTREE;
               }
+            }
 
-              @Override
-              public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
-                // TODO handle symlinks?
-                if (attributes.isSymbolicLink()) {
-                  log.warn("Skipped file (symlink): [{}]", file);
-                  return FileVisitResult.CONTINUE;
-                }
-                consumer.accept(file, attributes);
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+              // TODO handle symlinks?
+              if (attributes.isSymbolicLink()) {
+                log.warn("Skipped file (symlink): [{}]", file);
                 return FileVisitResult.CONTINUE;
               }
+              consumer.accept(file, attributes);
+              return FileVisitResult.CONTINUE;
+            }
 
-              @Override
-              public FileVisitResult visitFileFailed(Path file, IOException e) {
-                if (e instanceof AccessDeniedException) {
-                  log.warn("Skipped file (access denied): [{}]", file);
-                } else {
-                  log.error("Error visiting file: [{}]", file, e);
-                }
-                return FileVisitResult.CONTINUE;
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException e) {
+              if (e instanceof AccessDeniedException) {
+                log.warn("Skipped file (access denied): [{}]", file);
+              } else {
+                log.error("Error visiting file: [{}]", file, e);
               }
+              return FileVisitResult.CONTINUE;
+            }
 
-              @Override
-              public FileVisitResult postVisitDirectory(Path dir, IOException e) {
-                if (e != null) {
-                  log.error("Error visiting directory: [{}]", dir, e);
-                }
-                return FileVisitResult.CONTINUE;
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException e) {
+              if (e != null) {
+                log.error("Error visiting directory: [{}]", dir, e);
               }
-            });
-      } catch (IOException e) {
-        throw new UncheckedIOException(e);
-      }
-
-      for (ParallelWalk fork : forks) {
-        fork.join();
-      }
+              return FileVisitResult.CONTINUE;
+            }
+          });
+      return null;
     }
   }
 }
