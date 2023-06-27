9efb5ec3e4cf0e6092bfdb76818dc232ae7505e1
Will <willjoemolloy@gmail.com>
2023-06-27 13:41:49 +1200

    Replace `ExecutorService` with `StructuredTaskScope` (#28)
    New vthread feature. API is quite clean, like how it joins all forks.

---
+++ file-sync-core/src/main/java/com/willmolloy/sync/util/concurrent/VoidStructuredTaskScope.java
@@ -0,0 +1,36 @@
+package com.willmolloy.sync.util.concurrent;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import jdk.incubator.concurrent.StructuredTaskScope;
+
+/**
+ * {@link StructuredTaskScope} decorated to make the API cleaner for the use cases of this app
+ * ({@code void} tasks, implicit {@code join}, etc.).
+ *
+ * @author <a href=https://willmolloy.com>Will Molloy</a>
+ */
+public final class VoidStructuredTaskScope extends StructuredTaskScope<Void> {
+  private final StructuredTaskScope.ShutdownOnFailure delegate;
+
+  public VoidStructuredTaskScope(String name) {
+    this.delegate =
+        new StructuredTaskScope.ShutdownOnFailure(
+            name, Thread.ofVirtual().name("%s-worker-".formatted(name), 0).factory());
+  }
+
+  public void fork(Runnable task) {
+    delegate.fork(Executors.callable(task));
+  }
+
+  @Override
+  public void close() {
+    try {
+      delegate.join();
+      delegate.throwIfFailed();
+      delegate.close();
+    } catch (ExecutionException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
