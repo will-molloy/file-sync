7f447341cf526673bb75dc64de5f088d352492fa
Will Molloy <willjoemolloy@gmail.com>
2023-07-02 12:29:10 +1200

    Migrate settings and `file-sync-core/local`

---
+++ file-sync-local/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.ajoberstar.grgit.Grgit
+
+plugins {
+  id("com.google.cloud.tools.jib") version "3.3.2"
+  id("org.ajoberstar.grgit") version "5.2.0"
+}
+
+jib {
+  from {
+    image = "amazoncorretto:19"
+  }
+  to {
+    image = "ghcr.io/will-molloy/file-sync-local"
+  }
+  container {
+    mainClass = "com.willmolloy.sync.local.Main"
+    environment = mapOf("SOURCE_PATH" to "/source", "DESTINATION_PATH" to "/destination")
+    creationTime.set(getGitCommitTime())
+    jvmFlags = listOf("--enable-preview", "--add-modules", "jdk.incubator.concurrent")
+  }
+}
+
+fun getGitCommitTime(): String {
+  Grgit.open(mapOf("currentDir" to rootProject.rootDir)).use {
+    return it.head().dateTime.toInstant().toString()
+  }
+}
+
+dependencies {
+  implementation(project(":file-sync-core"))
+}
